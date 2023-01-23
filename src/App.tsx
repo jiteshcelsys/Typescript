a690386253086d00c8241640255f2c6b1382a144
jiteshcelsys <Jitesh.p@celestialsys.com>
2023-01-23 10:47:02 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { useState } from 'react';
-import './App.css';
-import LoweToupperCase from './components/LoweToupperCase';
-import GroceryList from './Grocery';
-import Select, { Selectoption } from './Select';
-
-const options =[
-  {label:"First", value:1},
-  {label:"second", value:2},
-  {label:"third", value:3},
-  {label:"fourth", value:4},
-  {label:"fifth", value:5},
-  {label:"sixth", value:6}
-
-]
+import "./App.css";
+import GroceryList from "./Grocery";
 
 function App() {
-  const [value,setValue] = useState<Selectoption|undefined>(options[0]);
-  
   return (
-    <div >
-      {/* <Select  options={options} value={value} xyz = {o=> setValue(o)}/> */}
-      <br></br>
-      {/* <GroceryList/> */}
-      <LoweToupperCase name='jitesh'/>
-    
-
-      </div>
+    <div>
+      <GroceryList />
+    </div>
   );
 }
 
